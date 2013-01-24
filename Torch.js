7937b94102d569a33bb72d21873029f0a01f3566
Lukas Beaton <lukas.beaton@gmail.com>
2013-01-24 13:32:14 -0500

    updates old PhoneGap plugin to with Cordova 2.3.0 on iOS6

--- Torch.js
+++ Torch.js
@@ -1,11 +1,11 @@
+// Torch.js
+// Cordova Plugin
 //
-//  Torch.js
-//  PhoneGap Plugin
-//
-//  Only supported in iOS4, and flash capable device (currently iPhone 4 only)
 //
 // Created by Shazron Abdullah May 26th 2011
+// Update by Lukas Beaton January 24, 2013
 //
+// This plugin was updated to work with iOS 6 and Cordova 2.3.0 (previously PhoneGap)
 
 function Torch()
 {
@@ -15,22 +15,49 @@ function Torch()
 	this.__defineGetter__("isOn", function() { return self._isOn; });	
 }
 
-Torch.prototype.turnOn = function()
-{
-	PhoneGap.exec("Torch.turnOn");
+
+
+Torch.prototype.turnOn = function(){
+  var fail = function() {}
+
+  if (typeof fail != "function")  {
+    console.log("Torch.turnOn failure: failure parameter not a function")
+    return
+  }
+
+  var success = function() {}
+
+  if (typeof success != "function")  {
+    console.log("Torch.turnOn success: failure parameter not a function")
+    return
+  }
+
+	cordova.exec(success, fail, "Torch", "turnOn", []);
 };
 
-Torch.prototype.turnOff = function()
-{
-	PhoneGap.exec("Torch.turnOff");
+Torch.prototype.turnOff = function(){
+  var fail = function() {}
+
+  if (typeof fail != "function")  {
+  console.log("Torch.turnOff failure: failure parameter not a function")
+  return
+  }
+
+  var success = function() {}
+
+  if (typeof success != "function")  {
+  console.log("Torch.turnOff success: failure parameter not a function")
+  return
+  }
+
+	cordova.exec(success, fail, "Torch", "turnOff", []);
 };
 
-Torch.install = function()
-{
+Torch.install = function(){
 	if(!window.plugins) {
 		window.plugins = {};
 	}
 	window.plugins.torch = new Torch();
 };
 
-PhoneGap.addConstructor(Torch.install);
+cordova.addConstructor(Torch.install);
